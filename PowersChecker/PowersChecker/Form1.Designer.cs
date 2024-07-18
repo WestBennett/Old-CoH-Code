d00d3383a0be8824f6c056a20c925143c87ae550
West Bennett <50267102+WestBennett@users.noreply.github.com>
2024-07-18 15:37:30 -0500

    Add files via upload

---
+++ PowersChecker/PowersChecker/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace PowersChecker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.cboEntity = new System.Windows.Forms.ComboBox();
+            this.lblEntity = new System.Windows.Forms.Label();
+            this.lblPowerSet = new System.Windows.Forms.Label();
+            this.cboPowerSet = new System.Windows.Forms.ComboBox();
+            this.lblPowerName = new System.Windows.Forms.Label();
+            this.cboPowerName = new System.Windows.Forms.ComboBox();
+            this.dgvAttributes = new System.Windows.Forms.DataGridView();
+            this.lblPrimaryAttributes = new System.Windows.Forms.Label();
+            this.lblAttributeMods = new System.Windows.Forms.Label();
+            this.dgvAttributeMods = new System.Windows.Forms.DataGridView();
+            this.ssMain = new System.Windows.Forms.StatusStrip();
+            this.ProgBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cmsMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exportDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttributes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttributeMods)).BeginInit();
+            this.ssMain.SuspendLayout();
+            this.cmsMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboEntity
+            // 
+            this.cboEntity.DisplayMember = "Entity";
+            this.cboEntity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEntity.FormattingEnabled = true;
+            this.cboEntity.Location = new System.Drawing.Point(54, 12);
+            this.cboEntity.Name = "cboEntity";
+            this.cboEntity.Size = new System.Drawing.Size(121, 21);
+            this.cboEntity.TabIndex = 0;
+            this.cboEntity.ValueMember = "Entity";
+            this.cboEntity.SelectedValueChanged += new System.EventHandler(this.CboEntity_SelectedValueChanged);
+            // 
+            // lblEntity
+            // 
+            this.lblEntity.AutoSize = true;
+            this.lblEntity.Location = new System.Drawing.Point(12, 15);
+            this.lblEntity.Name = "lblEntity";
+            this.lblEntity.Size = new System.Drawing.Size(36, 13);
+            this.lblEntity.TabIndex = 1;
+            this.lblEntity.Text = "Entity:";
+            // 
+            // lblPowerSet
+            // 
+            this.lblPowerSet.AutoSize = true;
+            this.lblPowerSet.Location = new System.Drawing.Point(181, 15);
+            this.lblPowerSet.Name = "lblPowerSet";
+            this.lblPowerSet.Size = new System.Drawing.Size(59, 13);
+            this.lblPowerSet.TabIndex = 3;
+            this.lblPowerSet.Text = "Power Set:";
+            // 
+            // cboPowerSet
+            // 
+            this.cboPowerSet.DisplayMember = "PowerSet";
+            this.cboPowerSet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPowerSet.Enabled = false;
+            this.cboPowerSet.FormattingEnabled = true;
+            this.cboPowerSet.Location = new System.Drawing.Point(246, 12);
+            this.cboPowerSet.Name = "cboPowerSet";
+            this.cboPowerSet.Size = new System.Drawing.Size(121, 21);
+            this.cboPowerSet.TabIndex = 2;
+            this.cboPowerSet.ValueMember = "PowerSet";
+            this.cboPowerSet.SelectedValueChanged += new System.EventHandler(this.CboPowerSet_SelectedValueChanged);
+            // 
+            // lblPowerName
+            // 
+            this.lblPowerName.AutoSize = true;
+            this.lblPowerName.Location = new System.Drawing.Point(373, 12);
+            this.lblPowerName.Name = "lblPowerName";
+            this.lblPowerName.Size = new System.Drawing.Size(71, 13);
+            this.lblPowerName.TabIndex = 5;
+            this.lblPowerName.Text = "Power Name:";
+            // 
+            // cboPowerName
+            // 
+            this.cboPowerName.DisplayMember = "PowerName";
+            this.cboPowerName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPowerName.Enabled = false;
+            this.cboPowerName.FormattingEnabled = true;
+            this.cboPowerName.Location = new System.Drawing.Point(450, 12);
+            this.cboPowerName.Name = "cboPowerName";
+            this.cboPowerName.Size = new System.Drawing.Size(121, 21);
+            this.cboPowerName.TabIndex = 4;
+            this.cboPowerName.ValueMember = "PowerName";
+            this.cboPowerName.SelectedValueChanged += new System.EventHandler(this.CboPowerName_SelectedValueChanged);
+            // 
+            // dgvAttributes
+            // 
+            this.dgvAttributes.AllowUserToAddRows = false;
+            this.dgvAttributes.AllowUserToDeleteRows = false;
+            this.dgvAttributes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvAttributes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAttributes.ContextMenuStrip = this.cmsMain;
+            this.dgvAttributes.Location = new System.Drawing.Point(15, 61);
+            this.dgvAttributes.MultiSelect = false;
+            this.dgvAttributes.Name = "dgvAttributes";
+            this.dgvAttributes.ReadOnly = true;
+            this.dgvAttributes.RowHeadersVisible = false;
+            this.dgvAttributes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAttributes.Size = new System.Drawing.Size(556, 150);
+            this.dgvAttributes.TabIndex = 6;
+            // 
+            // lblPrimaryAttributes
+            // 
+            this.lblPrimaryAttributes.AutoSize = true;
+            this.lblPrimaryAttributes.Location = new System.Drawing.Point(12, 45);
+            this.lblPrimaryAttributes.Name = "lblPrimaryAttributes";
+            this.lblPrimaryAttributes.Size = new System.Drawing.Size(80, 13);
+            this.lblPrimaryAttributes.TabIndex = 7;
+            this.lblPrimaryAttributes.Text = "Main Attributes:";
+            // 
+            // lblAttributeMods
+            // 
+            this.lblAttributeMods.AutoSize = true;
+            this.lblAttributeMods.Location = new System.Drawing.Point(12, 214);
+            this.lblAttributeMods.Name = "lblAttributeMods";
+            this.lblAttributeMods.Size = new System.Drawing.Size(94, 13);
+            this.lblAttributeMods.TabIndex = 8;
+            this.lblAttributeMods.Text = "Attribute Modifiers:";
+            // 
+            // dgvAttributeMods
+            // 
+            this.dgvAttributeMods.AllowUserToAddRows = false;
+            this.dgvAttributeMods.AllowUserToDeleteRows = false;
+            this.dgvAttributeMods.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvAttributeMods.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAttributeMods.ContextMenuStrip = this.cmsMain;
+            this.dgvAttributeMods.Location = new System.Drawing.Point(15, 230);
+            this.dgvAttributeMods.MultiSelect = false;
+            this.dgvAttributeMods.Name = "dgvAttributeMods";
+            this.dgvAttributeMods.ReadOnly = true;
+            this.dgvAttributeMods.RowHeadersVisible = false;
+            this.dgvAttributeMods.Size = new System.Drawing.Size(556, 150);
+            this.dgvAttributeMods.TabIndex = 9;
+            // 
+            // ssMain
+            // 
+            this.ssMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ProgBar,
+            this.StatusLabel});
+            this.ssMain.Location = new System.Drawing.Point(0, 396);
+            this.ssMain.Name = "ssMain";
+            this.ssMain.Size = new System.Drawing.Size(583, 22);
+            this.ssMain.TabIndex = 10;
+            this.ssMain.Text = "statusStrip1";
+            // 
+            // ProgBar
+            // 
+            this.ProgBar.Name = "ProgBar";
+            this.ProgBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.StatusLabel.Text = "Ready";
+            // 
+            // cmsMain
+            // 
+            this.cmsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportDataToolStripMenuItem});
+            this.cmsMain.Name = "cmsMain";
+            this.cmsMain.Size = new System.Drawing.Size(135, 26);
+            // 
+            // exportDataToolStripMenuItem
+            // 
+            this.exportDataToolStripMenuItem.Name = "exportDataToolStripMenuItem";
+            this.exportDataToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.exportDataToolStripMenuItem.Text = "Export Data";
+            this.exportDataToolStripMenuItem.Click += new System.EventHandler(this.ExportDataToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 418);
+            this.Controls.Add(this.ssMain);
+            this.Controls.Add(this.dgvAttributeMods);
+            this.Controls.Add(this.lblAttributeMods);
+            this.Controls.Add(this.lblPrimaryAttributes);
+            this.Controls.Add(this.dgvAttributes);
+            this.Controls.Add(this.lblPowerName);
+            this.Controls.Add(this.cboPowerName);
+            this.Controls.Add(this.lblPowerSet);
+            this.Controls.Add(this.cboPowerSet);
+            this.Controls.Add(this.lblEntity);
+            this.Controls.Add(this.cboEntity);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Powers Checker";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttributes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttributeMods)).EndInit();
+            this.ssMain.ResumeLayout(false);
+            this.ssMain.PerformLayout();
+            this.cmsMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboEntity;
+        private System.Windows.Forms.Label lblEntity;
+        private System.Windows.Forms.Label lblPowerSet;
+        private System.Windows.Forms.ComboBox cboPowerSet;
+        private System.Windows.Forms.Label lblPowerName;
+        private System.Windows.Forms.ComboBox cboPowerName;
+        private System.Windows.Forms.DataGridView dgvAttributes;
+        private System.Windows.Forms.Label lblPrimaryAttributes;
+        private System.Windows.Forms.Label lblAttributeMods;
+        private System.Windows.Forms.DataGridView dgvAttributeMods;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        public System.Windows.Forms.ToolStripProgressBar ProgBar;
+        public System.Windows.Forms.StatusStrip ssMain;
+        private System.Windows.Forms.ContextMenuStrip cmsMain;
+        private System.Windows.Forms.ToolStripMenuItem exportDataToolStripMenuItem;
+    }
+}
+
---
+++ PowersChecker/PowersChecker/Form1.cs
@@ -0,0 +1,267 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowersChecker
+{
+    public partial class MainForm : Form
+    {
+
+        bool DataLoaded = false;
+
+        DataSet MainData = null;
+        public MainForm()
+        {
+            InitializeComponent();
+            Show();
+            Cursor = Cursors.WaitCursor;
+
+            string FileName = Path.GetTempFileName();
+            File.WriteAllBytes(FileName, PKsPowersChecker.Properties.Resources.PlayerPowers);
+            MainData = GetDataSetFromExcel(FileName, true);
+            try
+            {
+                File.Delete(FileName);
+            }
+            catch
+            {
+                //Don't really care if we fail to delete
+            }
+
+            //MainData = GetDataSetFromExcel(PKsPowersChecker.Properties.Resources.PlayerPowers);
+
+            DataView view = new DataView(MainData.Tables["Powers"]);
+            DataTable distinctValues = view.ToTable(true, "Entity");
+            distinctValues.DefaultView.Sort = "Entity";
+            distinctValues = distinctValues.DefaultView.ToTable();
+            DataRow BlankRow = distinctValues.NewRow();
+            BlankRow["Entity"] = "";
+            distinctValues.Rows.InsertAt(BlankRow, 0);
+            cboEntity.DataSource = distinctValues;
+            DataLoaded = true;
+            Cursor = Cursors.Default;
+        }
+
+        public DataSet GetDataSetFromExcel(byte[] file, bool hasHeader = true)
+        {
+            DataSet ds = new DataSet();
+            using (var pck = new OfficeOpenXml.ExcelPackage())
+            {
+
+                using (var stream = new MemoryStream(file))
+                {
+                    pck.Load(stream);
+                }
+
+                foreach (ExcelWorksheet ws in pck.Workbook.Worksheets)
+                {
+                    DataTable tbl = new DataTable(ws.Name);
+                    foreach (var firstRowCell in ws.Cells[1, 1, 1, ws.Dimension.End.Column])
+                    {
+                        tbl.Columns.Add(hasHeader ? firstRowCell.Text : string.Format("Column {0}", firstRowCell.Start.Column));
+                    }
+                    var startRow = hasHeader ? 2 : 1;
+
+                    ProgBar.Minimum = 1;
+                    ProgBar.Maximum = ws.Dimension.End.Row;
+                    for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
+                    {
+                        ProgBar.Value = rowNum;
+                        StatusLabel.Text = "Adding Row " + rowNum.ToString() + " of " + ws.Dimension.End.Row.ToString() + " to DataSet from Sheet '" + ws.Name + "'";
+                        this.Refresh();
+                        Application.DoEvents();
+                        var wsRow = ws.Cells[rowNum, 1, rowNum, ws.Dimension.End.Column];
+                        DataRow row = tbl.Rows.Add();
+                        foreach (var cell in wsRow)
+                        {
+                            row[cell.Start.Column - 1] = cell.Text;
+                        }
+                    }
+                    ds.Tables.Add(tbl);
+                }
+            }
+
+            ProgBar.Visible = false;
+            StatusLabel.Text = "Ready";
+            return ds;
+        }
+
+        public DataSet GetDataSetFromExcel(string FileName, bool containsHeaderRow = false)
+        {
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + FileName + ";Extended Properties=\"Excel 12.0;HDR=No;IMEX=1\""; ;
+            DataSet dsReturn = new DataSet();
+
+            using (OleDbConnection excelConnection = new OleDbConnection(connectionString))
+            {
+                excelConnection.Open();
+
+                DataTable tableList = excelConnection.GetSchema("Tables");
+
+                if (tableList.Rows.Count == 0) return null;
+
+                foreach (DataRow dr in tableList.Rows)
+                {
+                    string sheetName = dr["TABLE_NAME"].ToString();
+                    using (OleDbCommand excelCommand = excelConnection.CreateCommand())
+                    {
+                        excelCommand.CommandText = "Select * From [" + sheetName + "]";
+                        excelCommand.CommandType = CommandType.Text;
+
+                        DataTable dt = new DataTable();
+                        using (OleDbDataReader oleExcelReader = excelCommand.ExecuteReader())
+                        {
+                            dt.Load(oleExcelReader);
+                            dt.TableName = sheetName.Replace("$", "");
+
+                            //Take the first column as the header column, if applicable
+                            if (containsHeaderRow)
+                            {
+                                foreach (DataColumn dc in dt.Columns)
+                                {
+                                    dc.ColumnName = dt.Rows[0][dc.ColumnName].ToString();
+                                }
+                                dt.Rows.Remove(dt.Rows[0]);
+                            }
+
+                            dsReturn.Tables.Add(dt);
+                        }
+                    }
+
+                }
+            }
+            return dsReturn;
+        }
+
+
+        private void GetAttributes(string EntityName, string PowerSet = null, string PowerName = null, string AttributeModName = null)
+        {
+            Cursor = Cursors.WaitCursor;
+            string Query = "Entity = '" + EntityName + "'";
+            if (PowerSet != null  && PowerSet != "") Query += " AND PowerSet = '" + PowerSet + "'";
+            if (PowerName != null && PowerName != "") Query += " AND PowerName = '" + PowerName + "'";
+
+            //Populate the Main Attributes and AttributeMod DGVs
+            DataTable MainAttributes = MainData.Tables["Powers"].Select(Query).CopyToDataTable<DataRow>();
+            DataLoaded = false;
+            MainAttributes.TableName = nameof(MainAttributes);
+            dgvAttributes.DataSource = MainAttributes;
+            dgvAttributes.ClearSelection();
+
+            if (AttributeModName != null) Query += " AND AttribModName = '" + AttributeModName + "'";
+            DataTable AttributeMods = MainData.Tables["AttribMods"].Select(Query).CopyToDataTable<DataRow>();
+            AttributeMods.TableName = nameof(AttributeMods);
+            dgvAttributeMods.DataSource = AttributeMods;
+            dgvAttributeMods.ClearSelection();
+            DataLoaded = true;
+            Cursor = Cursors.Default;
+        }
+
+        private void CboEntity_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (!DataLoaded) return;
+            if (cboEntity.SelectedValue == null || cboEntity.SelectedValue.ToString() == "")
+            {
+                cboPowerSet.DataSource = null;
+                cboPowerSet.Enabled = false;
+                cboPowerName.DataSource = null;
+                cboPowerName.Enabled = false;
+                dgvAttributes.DataSource = null;
+                dgvAttributeMods.DataSource = null;
+
+                return;
+            }
+            Cursor = Cursors.WaitCursor;
+            DataLoaded = false;
+            DataTable newValues = MainData.Tables["Powers"].Select("Entity = '" + cboEntity.SelectedValue + "'").CopyToDataTable<DataRow>();
+            DataView view = new DataView(newValues);
+            DataTable distinctValues = view.ToTable(true, "PowerSet");
+            distinctValues.DefaultView.Sort = "PowerSet";
+            distinctValues = distinctValues.DefaultView.ToTable();
+            DataRow BlankRow = distinctValues.NewRow();
+            BlankRow["PowerSet"] = "";
+            distinctValues.Rows.InsertAt(BlankRow, 0);
+            cboPowerSet.DataSource = distinctValues;
+            cboPowerSet.Enabled = true;
+
+            DataLoaded = true;
+            GetAttributes(cboEntity.SelectedValue.ToString());
+            Cursor = Cursors.Default;
+        }
+
+        private void CboPowerSet_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (!DataLoaded) return;
+            if (cboPowerSet.SelectedValue == null || cboPowerSet.SelectedValue.ToString() == "")
+            {
+                cboPowerName.DataSource = null;
+                cboPowerName.Enabled = false;
+                dgvAttributes.DataSource = null;
+                dgvAttributeMods.DataSource = null;
+                return;
+            }
+
+            Cursor = Cursors.WaitCursor;
+            DataLoaded = false;
+            DataTable newValues = MainData.Tables["Powers"].Select("Entity = '" + cboEntity.SelectedValue + "'" +
+                " AND PowerSet = '" + cboPowerSet.SelectedValue + "'").CopyToDataTable<DataRow>();
+            DataView view = new DataView(newValues);
+            DataTable distinctValues = view.ToTable(true, "PowerName");
+            distinctValues.DefaultView.Sort = "PowerName";
+            distinctValues = distinctValues.DefaultView.ToTable();
+            DataRow BlankRow = distinctValues.NewRow();
+            BlankRow["PowerName"] = "";
+            distinctValues.Rows.InsertAt(BlankRow, 0);
+            cboPowerName.DataSource = distinctValues;
+            cboPowerName.Enabled = true;
+
+            DataLoaded = true;
+            GetAttributes(cboEntity.SelectedValue.ToString(), cboPowerSet.SelectedValue.ToString());
+            Cursor = Cursors.Default;
+        }
+
+        private void CboPowerName_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (!DataLoaded) return;
+            if (cboPowerSet.SelectedValue == null || cboPowerSet.SelectedValue.ToString() == "")
+            {
+                dgvAttributes.DataSource = null;
+                dgvAttributeMods.DataSource = null;
+                return;
+            }
+
+            GetAttributes(cboEntity.SelectedValue.ToString(), cboPowerSet.SelectedValue.ToString(), cboPowerName.SelectedValue.ToString());
+        }
+
+        private void ExportDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DataGridView dgv = (DataGridView)((ContextMenuStrip)(((ToolStripMenuItem)sender).Owner)).SourceControl;
+            if (dgv.DataSource == null || ((DataTable)dgv.DataSource).Rows.Count == 0)
+            {
+                MessageBox.Show("No data found to export!", "No Data", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Where to save the file to?";
+            sfd.Filter = "Excel Files|*.xlsx";
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            if (File.Exists(sfd.FileName)) File.Delete(sfd.FileName);
+
+            using (ExcelPackage pck = new ExcelPackage(new FileInfo(sfd.FileName)))
+            {
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add(((DataTable)dgv.DataSource).TableName);
+                ws.Cells["A1"].LoadFromDataTable((DataTable)dgv.DataSource, true);
+                pck.Save();
+            }
+            MessageBox.Show("File '" + sfd.FileName + "' exported successfully!", "Export Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
