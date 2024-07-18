a9715ab8d37c4123944f97c1ae79cb4e96d2d2b4
WestleyBennett <50267102+WestleyBennett@users.noreply.github.com>
2024-07-18 15:24:19 -0500

    Add files via upload

---
+++ Def File Translator/Def File Translator/MainForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Def_File_Translator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnReadDefFile = new System.Windows.Forms.Button();
+            this.btnReadExcelFile = new System.Windows.Forms.Button();
+            this.btnWriteDefFile = new System.Windows.Forms.Button();
+            this.btnWriteExcelFile = new System.Windows.Forms.Button();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.cboObjects = new System.Windows.Forms.ComboBox();
+            this.lblObject = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReadDefFile
+            // 
+            this.btnReadDefFile.Location = new System.Drawing.Point(12, 12);
+            this.btnReadDefFile.Name = "btnReadDefFile";
+            this.btnReadDefFile.Size = new System.Drawing.Size(93, 23);
+            this.btnReadDefFile.TabIndex = 1;
+            this.btnReadDefFile.Text = "Read Def File";
+            this.btnReadDefFile.UseVisualStyleBackColor = true;
+            this.btnReadDefFile.Click += new System.EventHandler(this.BtnReadDefFile_Click);
+            // 
+            // btnReadExcelFile
+            // 
+            this.btnReadExcelFile.Location = new System.Drawing.Point(111, 12);
+            this.btnReadExcelFile.Name = "btnReadExcelFile";
+            this.btnReadExcelFile.Size = new System.Drawing.Size(99, 23);
+            this.btnReadExcelFile.TabIndex = 2;
+            this.btnReadExcelFile.Text = "Read Excel File";
+            this.btnReadExcelFile.UseVisualStyleBackColor = true;
+            this.btnReadExcelFile.Click += new System.EventHandler(this.BtnReadExcelFile_Click);
+            // 
+            // btnWriteDefFile
+            // 
+            this.btnWriteDefFile.Enabled = false;
+            this.btnWriteDefFile.Location = new System.Drawing.Point(216, 12);
+            this.btnWriteDefFile.Name = "btnWriteDefFile";
+            this.btnWriteDefFile.Size = new System.Drawing.Size(105, 23);
+            this.btnWriteDefFile.TabIndex = 3;
+            this.btnWriteDefFile.Text = "Write Def File";
+            this.btnWriteDefFile.UseVisualStyleBackColor = true;
+            this.btnWriteDefFile.Click += new System.EventHandler(this.BtnWriteDefFile_Click);
+            // 
+            // btnWriteExcelFile
+            // 
+            this.btnWriteExcelFile.Enabled = false;
+            this.btnWriteExcelFile.Location = new System.Drawing.Point(327, 12);
+            this.btnWriteExcelFile.Name = "btnWriteExcelFile";
+            this.btnWriteExcelFile.Size = new System.Drawing.Size(109, 23);
+            this.btnWriteExcelFile.TabIndex = 4;
+            this.btnWriteExcelFile.Text = "Write Excel File";
+            this.btnWriteExcelFile.UseVisualStyleBackColor = true;
+            this.btnWriteExcelFile.Click += new System.EventHandler(this.BtnWriteExcelFile_Click);
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(12, 71);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.Size = new System.Drawing.Size(879, 218);
+            this.dgvMain.TabIndex = 5;
+            // 
+            // cboObjects
+            // 
+            this.cboObjects.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboObjects.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboObjects.Enabled = false;
+            this.cboObjects.FormattingEnabled = true;
+            this.cboObjects.Location = new System.Drawing.Point(59, 41);
+            this.cboObjects.Name = "cboObjects";
+            this.cboObjects.Size = new System.Drawing.Size(830, 21);
+            this.cboObjects.TabIndex = 6;
+            this.cboObjects.SelectedValueChanged += new System.EventHandler(this.CboObjects_SelectedValueChanged);
+            // 
+            // lblObject
+            // 
+            this.lblObject.AutoSize = true;
+            this.lblObject.Location = new System.Drawing.Point(12, 44);
+            this.lblObject.Name = "lblObject";
+            this.lblObject.Size = new System.Drawing.Size(41, 13);
+            this.lblObject.TabIndex = 7;
+            this.lblObject.Text = "Object:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(903, 301);
+            this.Controls.Add(this.lblObject);
+            this.Controls.Add(this.cboObjects);
+            this.Controls.Add(this.dgvMain);
+            this.Controls.Add(this.btnWriteExcelFile);
+            this.Controls.Add(this.btnWriteDefFile);
+            this.Controls.Add(this.btnReadExcelFile);
+            this.Controls.Add(this.btnReadDefFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "PK\'s Def File Translator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnReadDefFile;
+        private System.Windows.Forms.Button btnReadExcelFile;
+        private System.Windows.Forms.Button btnWriteDefFile;
+        private System.Windows.Forms.Button btnWriteExcelFile;
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.ComboBox cboObjects;
+        private System.Windows.Forms.Label lblObject;
+    }
+}
+
---
+++ Def File Translator/Def File Translator/MainForm.cs
@@ -0,0 +1,268 @@
+﻿using Philotic_Knight;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Def_File_Translator
+{
+    public partial class MainForm : Form
+    {
+#pragma warning disable IDE0069 // Disposable fields should be disposed
+        //Disposed at form closing, don't know why that isn't good enough for Visual Studio, so suppressing
+        DataSet CurrentDataSet = null;
+        DataSet PowerSets = null;
+#pragma warning restore IDE0069 // Disposable fields should be disposed
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReadDefFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ClearForm();
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    ofd.Title = "Choose the file to open";
+                    if (ofd.ShowDialog() == DialogResult.Cancel || ofd.FileName == "" || !File.Exists(ofd.FileName)) return;
+
+                    Cursor = Cursors.WaitCursor;
+
+                    //Get the DefObjects from the file
+                    if (Path.GetExtension(ofd.FileName).ToUpper() == ".POWERS")
+                    {
+                        //Load all powersets in the same directory, if we're dealing with powers
+                        string[] powersets = Directory.GetFiles(Path.GetDirectoryName(ofd.FileName), "*.powersets");
+                        List<DefObject> AllPowerSets = new List<DefObject>();
+                        foreach (string powerset in powersets)
+                        {
+                            List<DefObject> ThisPowerset = DefMethods.GetDefObjectsFromDefFile(powerset);
+                            AllPowerSets.AddRange(ThisPowerset);
+                        }
+
+                        PowerSets = DefMethods.GetDataSetFromDefObjects(AllPowerSets);
+                    }
+                    List<DefObject> dObjects = DefMethods.GetDefObjectsFromDefFile(ofd.FileName);
+
+                    //Export these objects into a DataSet
+                    if (dObjects == null || dObjects.Count == 0)
+                    {
+                        MessageBox.Show("No Def Objects found in file.", "No Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+
+                    DataSet ds = DefMethods.GetDataSetFromDefObjects(dObjects);
+                    LoadData(ds);
+                    Text = "PK's Def File Translator - " + ofd.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                StandardError(ex);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private void BtnReadExcelFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ClearForm();
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    ofd.Title = "Choose the file to open";
+                    ofd.Filter = "Excel Files|*.xls";
+                    if (ofd.ShowDialog() == DialogResult.Cancel || ofd.FileName == "" || !File.Exists(ofd.FileName)) return;
+
+                    Cursor = Cursors.WaitCursor;
+
+                    //Get the DefObjects from the file
+                    List<DefObject> dObjects = DefMethods.GetDefObjectsFromExcelFile(ofd.FileName);
+
+                    //Export these objects into a DataSet
+                    if (dObjects == null || dObjects.Count == 0)
+                    {
+                        MessageBox.Show("No Def Objects found in file.", "No Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+
+                    DataSet ds = DefMethods.GetDataSetFromDefObjects(dObjects);
+                    LoadData(ds);
+                    Text = "PK's Def File Translator - " + ofd.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                StandardError(ex);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private void CboObjects_SelectedValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvMain.DataSource = null;
+                dgvMain.Enabled = false;
+                if (cboObjects.SelectedValue == null || cboObjects.SelectedValue.ToString() == "" ||
+                    CurrentDataSet == null || CurrentDataSet.Tables.Count == 0) return;
+
+                foreach (DataTable dt in CurrentDataSet.Tables)
+                {
+                    if (dt.Rows[0]["ObjectName"].ToString() == cboObjects.SelectedValue.ToString())
+                    {
+                        dgvMain.DataSource = dt;
+                        dgvMain.Enabled = true;
+                        dgvMain.Columns["ObjectName"].Visible = false;
+                        return;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                StandardError(ex);
+            }
+        }
+
+        private void BtnWriteDefFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (CurrentDataSet == null)
+                {
+                    MessageBox.Show("No Dataset found to write to file.", "No Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                using (SaveFileDialog sfd = new SaveFileDialog())
+                {
+                    sfd.Title = "Choose where to save the file to";
+                    sfd.Filter = "Def Files|*.def|All Files|*.*";
+                    if (sfd.ShowDialog() == DialogResult.Cancel || sfd.FileName == "") return;
+
+                    Cursor = Cursors.WaitCursor;
+                    DefMethods.WriteDataSetToDefFile(CurrentDataSet, sfd.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                StandardError(ex);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private void BtnWriteExcelFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (CurrentDataSet == null)
+                {
+                    MessageBox.Show("No Dataset found to write to file.", "No Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                using (SaveFileDialog sfd = new SaveFileDialog())
+                {
+                    sfd.Title = "Choose where to save the file to";
+                    sfd.Filter = "Excel Files|*.xls|All Files|*.*";
+                    if (sfd.ShowDialog() == DialogResult.Cancel || sfd.FileName == "") return;
+
+                    Cursor = Cursors.WaitCursor;
+                    DefMethods.WriteDataSetToExcelFile(CurrentDataSet, sfd.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                StandardError(ex);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void ClearForm()
+        {
+            try
+            {
+                Text = "PK's Def File Translator";
+                cboObjects.Enabled = false;
+                cboObjects.DataSource = null;
+                dgvMain.DataSource = null;
+                btnWriteDefFile.Enabled = false;
+                btnWriteExcelFile.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                StandardError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Load data into the form
+        /// </summary>
+        /// <param name="ds"></param>
+        /// <returns></returns>
+        private bool LoadData(DataSet ds)
+        {
+            try
+            {
+                if (ds.Tables.Count == 0) return false;
+                DataTable dt = new DataTable();
+                dt.Columns.Add("ObjectName");
+                dt.Rows.Add("");
+                foreach (DataTable dtSource in ds.Tables)
+                {
+                    dt.Rows.Add(dtSource.Rows[0]["ObjectName"].ToString());
+                }
+                cboObjects.ValueMember = "ObjectName";
+                cboObjects.DisplayMember = "ObjectName";
+                cboObjects.DataSource = dt;
+                cboObjects.Enabled = true;
+                CurrentDataSet = ds;
+                btnWriteDefFile.Enabled = true;
+                btnWriteExcelFile.Enabled = true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                StandardError(ex);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Present a standard error message to the user
+        /// </summary>
+        /// <param name="ex"></param>
+        private void StandardError(Exception ex)
+        {
+            MessageBox.Show("Program Error: " + ex.Message +
+                ", please send a screenshot of this error along with the offending file (if applicable) to The Philotic Knight." +
+                Environment.NewLine + Environment.NewLine + ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (CurrentDataSet != null) CurrentDataSet.Dispose();
+        }
+    }
+}
